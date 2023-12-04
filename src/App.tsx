7cfa39167f15ce4e2d3cd7de70644f89ee146a07
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-12-04 18:19:28 +0000

    Cria routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import CoffeeList from './pages/coffee-list';
+import NavBar from './components/nav-bar';
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/coffees" element={ <CoffeeList /> } />
+      </Routes>
     </>
   );
 }
